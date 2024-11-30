78fd1b7ff8dec35c0d29747c02a1512c8b10bf90
Popeye88 <121196522+Gladius33@users.noreply.github.com>
2024-11-30 14:31:12 +0100

    Update axiosConfig.js

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -16,7 +16,6 @@ axios.defaults.headers.common['Content-Type'] = 'application/json';
 
 axios.interceptors.request.use(
   config => {
-    // Vous pouvez ajouter d'autres configurations ici si nécessaire
     return config;
   },
   error => {
@@ -27,18 +26,13 @@ axios.interceptors.request.use(
 axios.interceptors.response.use(
   response => response,
   error => {
-    // Gestion des erreurs globales
     if (error.response) {
-      // Le serveur a répondu avec un statut différent de 2xx
       if (error.response.status === 401) {
-        // Déconnexion utilisateur ou autre action
-      }
+        }
       return Promise.reject(error.response);
     } else if (error.request) {
-      // La requête a été faite mais aucune réponse n'a été reçue
       return Promise.reject({ message: 'No response received from server' });
     } else {
-      // Une autre erreur s'est produite lors de la configuration de la requête
       return Promise.reject({ message: 'Error: ' + error.message });
     }
   }
